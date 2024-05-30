3442882f1a44ce130393a66c5937e16bd33f99a3
Gino Cazzola <ginex.bluex@gmail.com>
2024-05-30 12:09:19 -0300

    Validations

---
+++ src/middlewares/validations.js
@@ -0,0 +1,23 @@
+import { validationResult } from "express-validator";
+
+export const validationErrorResponse = (req, res, next) => {
+//3 parámetros
+       const errors = validationResult(req);
+       
+       if (!errors.isEmpty()) {
+          const errorList = errors 
+          .array()
+          .map( err => ({[err.path]: err.msg}))
+       return res.status(400).json({
+        ok: false,
+        errors: errorList
+
+
+       })
+
+    } else {
+        next()
+    }
+
+
+}
