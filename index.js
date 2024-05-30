46eff429ab18cbccb97ffe16f9ed911643fd15a7
Gino Cazzola <ginex.bluex@gmail.com>
2024-05-30 12:38:46 -0300

    dotenv

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 
 import express from "express"
+import cors from "cors"
 
 import endpointRoute from "./src/routes/firstEndpoint.routes.js"
 import authRoutes from "./src/routes/auth.routes.js"
 
-
 import { dbConection } from "./database/dbConection.js"
+import dotenv from "dotenv"
 
 const PORT = 4000
 
 const appServer = async () => {
-
+      dotenv.config()
       await dbConection()
+      console.log()
 
       const server = express()
 
       server.use(express.json())
+
+      server.use(cors())
 
       server.use("/api/endpoint", endpointRoute)
       server.use("/api/auth", authRoutes)
