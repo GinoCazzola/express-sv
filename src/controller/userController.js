1bda1813131b27c152410fb0f9bfab852b9cc5a3
Gino Cazzola <ginex.bluex@gmail.com>
2024-05-29 21:52:50 -0300

    User updates

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,15 +1,23 @@
 import User from "../../models/User.js";
+import * as bcrypt from 'bcrypt';
 
 export const createUser = async (req, res) => {
     const { body } = req;
     try {
-        const user = await User.create(body);
+        const hashPassword = await bcrypt.hash(body.password, 10)
+
+        const user = await User.create({
+        ...body,
+        password: hashPassword
+        })
+
         res.json({
             ok: true,
             user,
             msg: "Usuario creado correctamente."
         });
     } catch (error) {
+        console.log(error)
         res.status(500).json({
             ok: false,
             msg: "Ocurrió un error en el servidor."
